839f26514c724db49bafdc76febfe507ba4754bb
Iryna Meleshko <irishka.m.k@ukr.net>
2024-04-27 22:51:46 +0300

    initial

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price * deliveryFee;
+    const totalPrice = price + deliveryFee;
     console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
 }
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,17 @@
 function getElementWidth(content, padding, border) {
-    console.log(content + padding + border);
-    return getElementWidth;
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderThickness = parseFloat(border);
+
+    if (isNaN(contentWidth) || isNaN(paddingWidth) || isNaN(borderThickness)) {
+        return "Invalid input. Please provide values in format 'Npx'.";
+    }
+
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderThickness;
+
+    return totalWidth;
 }
+
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
